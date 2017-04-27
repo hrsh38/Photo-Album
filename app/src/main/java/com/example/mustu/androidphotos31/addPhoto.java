c31b595aa82a8115d3680842d1918c40864c63fa
Harsh Patel <Harsh Patel>
2017-04-27 15:04:58 -0400

    photoclass

---
+++ app/src/main/java/com/example/mustu/androidphotos31/addPhoto.java
@@ -0,0 +1,20 @@
+package com.example.mustu.androidphotos31;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+/**
+ * Created by harsh on 4/27/2017.
+ */
+
+public class addPhoto extends AppCompatActivity {
+    ImageView imageToUpload;
+    Button add;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
